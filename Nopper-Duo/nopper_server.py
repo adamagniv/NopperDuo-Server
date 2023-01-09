de24d82eeba8c5cc419c8864c9a2a670f6cfb10c
adamagniv <adam.aronov@gmail.com>
2023-01-09 15:54:02 +0200

    added prints on serveer side

--- Nopper-Duo/nopper_server.py
+++ Nopper-Duo/nopper_server.py
@@ -37,11 +37,11 @@ class NopperServer(BaseHTTPRequestHandler):
         if (self.path.strip('/') == "login"):
             self.get_login()
         elif (self.path.strip('/') == "wait_for_partner"):
-            self.get_wait_for_partner(self.headers)
+            self.get_wait_for_partner()
         elif (self.path.strip('/') == "menu_done_wait"):
-            self.get_menu_done_wait(self.headers)
+            self.get_menu_done_wait()
         elif (self.path.strip('/') == "get_partner_line"):
-            self.get_partner_line(self.headers)
+            self.get_partner_line()
         elif (self.path.strip('/') == "disconnect"):
             self.get_disconnect()
         else:
@@ -75,11 +75,15 @@ class NopperServer(BaseHTTPRequestHandler):
                     role = MASTER
                     session_id = str(uuid4())
                     session_id_dic[0][session_id] = NopperSession()
+                    print("NDuo-Server: new {} connected sessionID {}".format(role, session_id))
                 elif count_connection == 1:
                     role = SLAVE
                     session_id_dic[1][session_id] = NopperSession()
+                    print("NDuo-Server: new {} connected sessionID {}".format(role, session_id))
                 else:
                     self.wfile.write(b"Can't have more than two participants")
+                    print("NDuo-Server: problematic login attempt server_accept_clients {} count_connection {}"\
+                        .format(str(server_accept_clients), str(count_connection)))
                     return
 
                 count_connection += 1
@@ -87,66 +91,74 @@ class NopperServer(BaseHTTPRequestHandler):
                     server_accept_clients = False
             else:
                 self.wfile.write(b"Server doesn't listen to new connections now")
+                print("NDuo-Server: rejected new client login")
                 return
-        
+
         self.wfile.write("{}:{}".format(role, session_id).encode("utf-8"))
 
-    def get_wait_for_partner(self, headers):
+    def get_wait_for_partner(self):
         global session_id_dic
         self.send_response(200)
         self.end_headers()
 
         try:
-            req_role, req_session_id = headers["X-NopperId"].split(':')
+            req_role, req_session_id = self.headers["X-NopperId"].split(':')
         except Exception:
             self.wfile.write(b"Invalid session ID")
+            print("NDuo-Server: get_wait_for_partner couldn't parse X-NopperId.")
             return
 
         try:
-            req_wait = int(headers["X-NopperTimeout"].strip())
+            req_wait = int(self.headers["X-NopperTimeout"].strip())
         except Exception:
             self.wfile.write(b"Invalid timeout")
+            print("NDuo-Server: get_wait_for_partner couldn't parse X-NopperTimeout.")
             return
 
         role_index = 0 if req_role == MASTER else 1
         current_session = session_id_dic[role_index].get(req_session_id)
         if current_session is None or current_session.state > NopperState.paired:
             self.wfile.write(b"fail")
-
+            print("NDuo-Server: client asked to pair in invalid session state.\
+                 role {} | sessionID {}".format(req_role, req_session_id))
             return
 
         for _ in range(0, req_wait):
             if (req_session_id in session_id_dic[role_index ^ 1]):
                 current_session.state = NopperState.paired
                 self.wfile.write(b"ok")
-
+                print("NDuo-Server: pairing session {} done".format(req_session_id))
                 return
             sleep(1)
 
         self.wfile.write(b"timeout")
+        print("NDuo-Server: pairing session {} timeout".format(req_session_id))
 
-    def get_menu_done_wait(self, headers):
+    def get_menu_done_wait(self):
         global session_id_dic
         self.send_response(200)
         self.end_headers()
 
         try:
-            req_role, req_session_id = headers["X-NopperId"].split(':')
+            req_role, req_session_id = self.headers["X-NopperId"].split(':')
         except Exception:
             self.wfile.write(b"Invalid session ID")
+            print("NDuo-Server: get_menu_done_wait couldn't parse X-NopperId.")
             return
 
         try:
-            req_wait = int(headers["X-NopperTimeout"].strip())
+            req_wait = int(self.headers["X-NopperTimeout"].strip())
         except Exception:
             self.wfile.write(b"Invalid timeout")
+            print("NDuo-Server: get_menu_done_wait couldn't parse X-NopperTimeout.")
             return
 
         role_index = 0 if req_role == MASTER else 1
         current_session = session_id_dic[role_index].get(req_session_id)
         if current_session is None or current_session.state < NopperState.paired:
             self.wfile.write(b"fail")
-
+            print("NDuo-Server: client menu_done in invalid session state.\
+                 role {} | sessionID {}".format(req_role, req_session_id))
             return
 
         current_session.state = NopperState.ready_to_send_line if req_role == MASTER else NopperState.waiting_to_receive_line
@@ -155,38 +167,45 @@ class NopperServer(BaseHTTPRequestHandler):
             parter_session = self.get_partner_session(req_role, req_session_id)
             if parter_session is None:
                 self.wfile.write(b"Partner disconncted")
+                print("NDuo-Server: client partner disconnected while waiting on menu_done. \
+                    Disconnecting {}:{}".format(req_role, req_session_id))
                 self.do_disconnect()
                 return
 
             if parter_session.state > NopperState.paired:
                 self.wfile.write(b"ok")
-
+                print("NDuo-Server: both sides ready to begin session {}".format(req_session_id))
                 return
             sleep(1)
 
         self.wfile.write(b"timeout")
+        print("NDuo-Server: menu_done session {} timeout".format(req_session_id))
 
-    def get_partner_line(self, headers):
+    def get_partner_line(self):
         global session_id_dic
         self.send_response(200)
         self.end_headers()
 
         try:
-            req_role, req_session_id = headers["X-NopperId"].split(':')
+            req_role, req_session_id = self.headers["X-NopperId"].split(':')
         except Exception:
             self.wfile.write(b"Invalid session ID")
+            print("NDuo-Server: get_partner_line couldn't parse X-NopperId.")
             return
 
         try:
-            req_wait = int(headers["X-NopperTimeout"].strip())
+            req_wait = int(self.headers["X-NopperTimeout"].strip())
         except Exception:
             self.wfile.write(b"Invalid timeout")
+            print("NDuo-Server: get_partner_line couldn't parse X-NopperTimeout.")
             return
 
         role_index = 0 if req_role == MASTER else 1
         current_session = session_id_dic[role_index].get(req_session_id)
         if current_session is None or current_session.state != NopperState.waiting_to_receive_line:
             self.wfile.write(b"fail")
+            print("NDuo-Server: client get_partner_line in invalid session state.\
+                 role {} | sessionID {}".format(req_role, req_session_id))
             return
 
         try:
@@ -194,6 +213,8 @@ class NopperServer(BaseHTTPRequestHandler):
                 parter_session = self.get_partner_session(req_role, req_session_id)
                 if parter_session is None:
                     self.wfile.write(b"Partner disconncted")
+                    print("NDuo-Server: client partner disconnected while waiting on partner_line. \
+                    Disconnecting {}:{}".format(req_role, req_session_id))
                     self.do_disconnect()
                     return
 
@@ -206,6 +227,8 @@ class NopperServer(BaseHTTPRequestHandler):
                 sleep(1)
         except:
             self.wfile.write(b"Partner disconnected")
+            print("NDuo-Server: in except while waiting on partner_line. \
+                    Disconnecting {}:{}".format(req_role, req_session_id))
             self.do_disconnect()
 
     def accept_line(self):
@@ -217,17 +240,22 @@ class NopperServer(BaseHTTPRequestHandler):
             req_role, req_session_id = self.headers["X-NopperId"].split(':')
         except Exception:
             self.wfile.write(b"Invalid session ID")
+            print("NDuo-Server: accept_line couldn't parse X-NopperId.")
             return
 
         role_index = 0 if req_role == MASTER else 1
         current_session = session_id_dic[role_index].get(req_session_id)
         if current_session is None or current_session.state != NopperState.ready_to_send_line:
             self.wfile.write(b"fail")
+            print("NDuo-Server: client accept_line in invalid session state.\
+                 role {} | sessionID {}".format(req_role, req_session_id))
             return
 
         parter_session = self.get_partner_session(req_role, req_session_id)
         if parter_session is None:
             self.wfile.write(b"Partner disconncted")
+            print("NDuo-Server: client partner disconnected while accept_line. \
+                    Disconnecting {}:{}".format(req_role, req_session_id))
             self.do_disconnect()
             return
 
@@ -254,6 +282,7 @@ class NopperServer(BaseHTTPRequestHandler):
             req_role, req_session_id = self.headers["X-NopperId"].split(':')
         except Exception:
             self.wfile.write(b"Invalid session ID")
+            print("NDuo-Server: do_disconnect couldn't parse X-NopperId.")
             return
 
         role_index = 0 if req_role == MASTER else 1
@@ -262,10 +291,13 @@ class NopperServer(BaseHTTPRequestHandler):
             with login_mutex:
                 del session_id_dic[role_index][req_session_id]
                 count_connection -= 1
+                print("NDuo-Server: disconnected {}:{}".format(req_role, req_session_id))
 
                 if count_connection == 0 and not server_accept_clients:
                     server_accept_clients = True
                     session_id = None
                     session_id_dic = [{}, {}]
+                    print("NDuo-Server: session {} ended on both sides, accepting new couple.".format(req_session_id))
         else:
             self.wfile.write(b"Invalid role for session ID")
+            print("NDuo-Server: no {}:{} to disconnect".format(req_role, req_session_id))
